77adb2302c756df60f02f374c82a0abb4da4c6f2
vorahardik7 <vorahardik056@gmail.com>
2025-02-02 14:46:59 -0800

    added a schema for the user details

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from .. import db
+from datetime import datetime
 
 class Video(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -11,3 +12,11 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     video_id = db.Column(db.Integer, db.ForeignKey('video.id'), nullable=False)
     content = db.Column(db.String(200), nullable=False)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.timezone.utc)
+    updated_at = db.Column(db.DateTime, default=datetime.timezone.utc, onupdate=datetime.timezone.utc)
